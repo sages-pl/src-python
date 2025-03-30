4ec34682885872ee9ac9a5dc6e36784fb05309d2
Matt Harasymczuk <matt@AstroMatt-MacBookPro-M3.local>
2025-03-30 20:32:56 +0200

    Update

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,2 +1,2 @@
-curl https://raw.githubusercontent.com/sages-pl/src-python/refs/heads/main/bin/setup-system.sh |bash
-curl https://raw.githubusercontent.com/sages-pl/src-python/refs/heads/main/bin/setup-project.sh |bash
+curl https://raw.githubusercontent.com/sages-pl/src-python/refs/heads/main/bin/setup-sys.sh |bash
+curl https://raw.githubusercontent.com/sages-pl/src-python/refs/heads/main/bin/setup-proj.sh |bash
